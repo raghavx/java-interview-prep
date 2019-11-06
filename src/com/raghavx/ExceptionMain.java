1e12e78ef48d0ce486030a632c2148729d35dd91
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-06 19:00:36 +0530

    excption learning started

---
+++ src/com/raghavx/ExceptionMain.java
@@ -0,0 +1,20 @@
+package com.raghavx;
+
+public class ExceptionMain {
+/*
+ * git status 
+ * git add . 
+ * git commit -m "message "
+ * git push 
+ * 
+ */
+	public ExceptionMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
