25356988a6449347e7a90285be103ecfceac8ef9
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-06 19:12:00 +0530

    how to avoid runtime exception demo

--- src/com/raghavx/ExceptionMain.java
+++ src/com/raghavx/ExceptionMain.java
@@ -4,13 +4,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ExceptionMain {
-/*
- * git status 
- * git add . 
- * git commit -m "message "
- * git push 
- * 
- */
+	/*
+	 * git status git add . git commit -m "message " git push
+	 * 
+	 */
 	public ExceptionMain() {
 		// TODO Auto-generated constructor stub
 	}
@@ -18,18 +15,17 @@ public class ExceptionMain {
 	public static void main(String[] args) {
 
 		int a = 10, b = 0;
-		//System.out.println(a/b);
-		
-		
+		if (b != 0) {
+			System.out.println(a / b);
+		}
+
 		try {
 			FileInputStream fis = new FileInputStream("");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Object obj = null;
 		obj.toString();
-		
 
 	}
 
