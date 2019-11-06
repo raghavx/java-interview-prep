f4718d9d6e79cfedeefbc0e93e1028d489afe0da
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-06 19:36:17 +0530

    multiple catch

---
+++ src/com/raghavx/MultipleExceptionHandling.java
@@ -0,0 +1,33 @@
+package com.raghavx;
+
+import java.io.IOException;
+
+public class MultipleExceptionHandling {
+
+	public MultipleExceptionHandling() {
+
+	}
+
+	public void badAirQualityException() throws BadAirQualityException {
+		throw new BadAirQualityException();
+	}
+
+	public void iOException() throws IOException {
+		throw new IOException();
+	}
+
+	public static void main(String[] args) {
+		MultipleExceptionHandling meh = new MultipleExceptionHandling();
+		try {
+			meh.badAirQualityException();
+			meh.iOException();
+		} catch (BadAirQualityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
